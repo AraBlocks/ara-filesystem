b7bf00fe12a4be94d8418f5e12d4e929a05acb2e
cckelly21 <cckelly21@gmail.com>
2018-06-18 13:28:41 -0400

    test(): begin ava testrunner setup

---
+++ test/aid.js
@@ -0,0 +1,15 @@
+const debug = require('debug')('ara-filesystem:test:aid')
+const test = require('ava')
+const aid = require('../aid')
+
+test("create(publicKey)", async (t) => {
+
+})
+
+test("archive(identity, opts)", async (t) => {
+
+})
+
+test("resolve(did, opts = {})", async (t) => {
+  
+})
