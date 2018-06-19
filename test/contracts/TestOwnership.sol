fe4be939b53ea6985c67f469dad040aebbe86ebb
Charles Kelly <cckelly21@gmail.com>
2018-06-19 19:14:44 -0400

    chore(): sol testing setup

---
+++ test/contracts/TestOwnership.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.4.0;
+
+import "truffle/Assert.sol";
+import "truffle/DeployedAddresses.sol";
+import "../../contracts/Ownership.sol";
+
+contract TestOwnership {
+	
+	// TODO(cckelly)
+
+}
---
+++ test/ownership.js
@@ -0,0 +1,21 @@
+/* eslint quotes: off */
+
+const test = require('ava')
+const { kTestDid } = require('./_constants')
+const { publish, resolve } = require('../ownership')
+
+test(`publish({
+	identity = '',
+	root = '',
+	signature = ''
+}= {})`, async (t) => {
+
+	// TODO(cckelly)
+
+})
+
+test('resolve({identity = ''} = {})', async (t) => {
+
+	// TODO(cckelly)
+
+})
