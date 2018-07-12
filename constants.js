9651dbca9325050b91e9eeb6d24f6018cfe04956
cckelly21 <cckelly21@gmail.com>
2018-07-12 16:29:22 -0400

    feat(): only commit metadata tree and sig

--- constants.js
+++ constants.js
@@ -14,21 +14,13 @@ module.exports = {
   kPriceAddress: '0xdda6327139485221633a1fcd65f4ac932e60a2e1',
 
   kFileMappings: {
-    kContentTree: {
-      name: 'content/tree',
-      index: 0
-    },
-    kContentSignatures: {
-      name: 'content/signatures',
-      index: 1
-    },
     kMetadataTree: {
       name: 'metadata/tree',
-      index: 2
+      index: 0
     },
     kMetadataSignatures: {
       name: 'metadata/signatures',
-      index: 3
+      index: 1
     }
   }
 }
