4dfde313c4ec91ee9059426dff4a36b6a091a564
cckelly21 <cckelly21@gmail.com>
2018-07-16 16:39:54 -0400

    feat(): destroy cli, delete toilet db file

--- destroy.js
+++ destroy.js
@@ -2,22 +2,48 @@ const debug = require('debug')('ara-filesystem:destroy')
 const { destroyCFS } = require('cfsnet/destroy')
 const { validate } = require('./util')
 const { createAFSKeyPath } = require('./key-path')
-const fs = require('fs')
+const { access } = require('fs')
+const { basename, resolve } = require('path')
 const rimraf = require('rimraf')
 const pify = require('pify')
 
+const kNodesDir = 'nodes'
+
 async function destroy({
   did = '',
+  mnemonic = '',
   password = ''
 } = {}) {
   await validate(did, password, 'destroy')
-  const path = createAFSKeyPath(did)
+  let path = createAFSKeyPath(did)
+  const hash = basename(path)
 
   try {
-    await pify(fs.access)(path)
+    // delete AFS on disk
+    await pify(access)(path)
     await pify(rimraf)(path)
+
+    // destroy AFS identity
+
   } catch (err) {
     throw new Error(err)
+  }
+
+  path = resolve(path.replace(hash, ''), kNodesDir, hash)
+
+  // delete AFS toilet db file
+  try {
+    await pify(access)(path)
+    await pify(rimraf)(path)
+  } catch (err) {
+    debug('db file at %s does not exist', path)
+  }
+
+  // destroy AFS identity
+  try {
+
+  } catch (err) {
+    throw new Error()
   }
 }
 
