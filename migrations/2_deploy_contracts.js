379230ea7530c4c24ca97e777efe80c2a93d2d4c
bplaster <brandon@odestooats.com>
2018-09-06 14:41:36 -0400

    Bap unarchive fix (#91)
    * fix(unarchive): fixed oncomplete, and const path
    
    * chore(contracts): removed old contract folders

--- migrations/2_deploy_contracts.js
+++
@@ -1,9 +0,0 @@
-/* eslint no-undef: "off" */
-
-const Storage = artifacts.require('./Storage.sol')
-const Price = artifacts.require('./Price.sol')
-
-module.exports = (deployer) => {
-  deployer.deploy(Storage)
-  deployer.deploy(Price)
-}
