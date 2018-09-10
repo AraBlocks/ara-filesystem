5bdcb183355a144d5ee435eb1a2698feb91e776a
Eric Jiang <eric@littlstar.com>
2018-09-10 14:09:12 -0400

    test(): unarchive tests working

--- test/unarchive.js
+++ test/unarchive.js
@@ -1,50 +1,90 @@
-const test = require('ava')
-const context = require('ara-context')()
+const mirror = require('mirror-folder')
+const crypto = require('ara-crypto')
 const aid = require('ara-identity')
-const fs = require('fs')
+const mkdirp = require('mkdirp')
 const pify = require('pify')
-const { create, unarchive, add } = require('../')
-const { writeIdentity } = require('ara-identity/util')
-const { kPassword: password } = require('./_constants')
+const test = require('ava')
+const fs = require('fs')
 
-const getDid = t => t.context.did
+const {
+  kTestOwnerDidNoMethod,
+  kPassword: password
+} = require('./_constants')
+
+const {
+  resolve,
+  parse
+} = require('path')
+
+const {
+  create,
+  unarchive,
+  add
+} = require('../')
+
 const kTestFilename = 'hello.txt'
 
+const getAFS = (t) => {
+  const { afs } = t.context
+  return afs
+}
+
 test.before(async (t) => {
-  const identity = await aid.create({ context, password })
-  await writeIdentity(identity)
-  let { publicKey } = identity
-  publicKey = publicKey.toString('hex')
-  const { afs } = await create({ owner: publicKey, password })
-  t.context = { did: afs.did }
+  const publicKey = Buffer.from(kTestOwnerDidNoMethod, 'hex')
+  const hash = crypto.blake2b(publicKey).toString('hex')
+  const path = `${__dirname}/fixtures/identities`
+  const ddoPath = resolve(path, hash, 'ddo.json')
+  const ddo = JSON.parse(await pify(fs.readFile)(ddoPath, 'utf8'))
+  const identityPath = aid.createIdentityKeyPath(ddo)
+  const parsed = parse(identityPath)
+  await pify(mkdirp)(parsed.dir)
+  await pify(mirror)(resolve(path, hash), identityPath)
+  t.context = { ddo, did: kTestOwnerDidNoMethod }
 })
 
-test.serial('unarchive() invalid opts', async (t) => {
-  const did = getDid(t)
+test.beforeEach(async (t) => {
+  const { did, ddo } = t.context
+  let afs
+  try {
+    // eslint-disable-next-line semi
+    ({ afs } = await create({ owner: did, password, ddo }));
+  } catch (err) {
+    console.log(err)
+  }
+  t.context = { afs }
+})
+
+test('unarchive() invalid opts', async (t) => {
+  const afs = getAFS(t)
+  const { did } = afs
 
   // validate DID
-  await t.throws(unarchive(), TypeError)
-  await t.throws(unarchive({ }), TypeError)
-  await t.throws(unarchive({ did: 1234 }), TypeError)
+  await t.throwsAsync(unarchive(), TypeError)
+  await t.throwsAsync(unarchive({ }), TypeError)
+  await t.throwsAsync(unarchive({ did: 1234 }), TypeError)
 
   // validate path
-  await t.throws(unarchive({ did, path: 123 }), TypeError)
+  await t.throwsAsync(unarchive({ did, path: 123 }), TypeError)
 })
 
-test.serial('unarchive() empty AFS', async (t) => {
-  const did = getDid(t)
-  await t.throws(unarchive({ did }), Error)
+test('unarchive() empty AFS', async (t) => {
+  const afs = getAFS(t)
+  const { did } = afs
+  await t.throwsAsync(unarchive({ did }), Error)
 })
 
-test.serial('unarchive() valid unarchive', async (t) => {
-  const did = getDid(t)
+test('unarchive() valid unarchive', async (t) => {
+  const afs = getAFS(t)
+  const { did } = afs
   // create test file and add to test AFS
-  await pify(fs.writeFile)(kTestFilename, 'Hello World!')
+  console.log(did)
+  console.log(resolve(kTestFilename))
+  await pify(fs.writeFile)(resolve(kTestFilename), 'Hello World!')
   await add({ did, password, paths: [ kTestFilename ] })
 
   // test unarchive to cwd
   await unarchive({ did })
-  await t.notThrows(pify(fs.access)(kTestFilename))
+  await t.notThrowsAsync(pify(fs.access)(kTestFilename))
   const result = await pify(fs.readFile)(kTestFilename, 'utf8')
   t.is(result, 'Hello World!')
 })
