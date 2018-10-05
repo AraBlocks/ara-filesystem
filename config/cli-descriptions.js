9a6d85984ef0fd5e72dde5b8e6a03290866e7b87
Madeline Cameron <github@maddie.today>
2018-10-05 12:01:59 -0400

    refactor(bin/afs): Improve CLI

---
+++ config/cli-descriptions.js
@@ -0,0 +1,7 @@
+module.exports = {
+  network: 'Network name in keyring',
+  secret: 'Shared secret for the keyring',
+  keyring: 'Path to the keyring',
+  did: 'DID of the AFS',
+  identity: 'Ara identity (DID) of the owner',
+}
