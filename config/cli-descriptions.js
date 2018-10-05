8781f6f1e00b16b2f10c416006bef69907054e11
Madeline Cameron <github@maddie.today>
2018-10-05 16:50:47 -0400

    refactor(bin/afs): Use imported descriptions

--- config/cli-descriptions.js
+++ config/cli-descriptions.js
@@ -4,4 +4,7 @@ module.exports = {
   keyring: 'Path to the keyring',
   did: 'DID of the AFS',
   identity: 'Ara identity (DID) of the owner',
+  password: 'Password for the given identity',
+  yes: 'Continue without confirming prompts',
+  price: 'Cost associated with an AFS (in ARA)'
 }
