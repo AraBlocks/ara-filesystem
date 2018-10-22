f64a9e19d81c45645e443a316b520c5cb8c4f933
Eric Jiang <eric@littlstar.com>
2018-10-22 14:26:01 -0400

    chore(): deprecation msgs

--- price.js
+++ price.js
@@ -1,7 +1,7 @@
 const { abi } = require('ara-contracts/build/contracts/AFS.json')
 const debug = require('debug')('ara-filesystem:price')
-const { kAidPrefix } = require('./constants')
 const { token, price } = require('ara-contracts')
+const { kAidPrefix } = require('./constants')
 const util = require('util')
 
 const {
@@ -20,8 +20,8 @@ const {
 } = require('ara-util')
 
 module.exports = {
-  estimateSetPriceGasCost: util.deprecate(async (opts) => { return price.estimateSetPriceGasCost(opts) }, 'afs.estimateSetPriceGasCost() is deprecated. Use act.estimateSetPriceGasCost() instead.'),
-  removePriceTier: util.deprecate(async (opts) => { return price.removePriceTier(opts) }, 'afs.removePriceTier() is deprecated. Use act.removePriceTier() instead.'),
-  setPrice: util.deprecate(async (opts) => { return price.setPrice(opts) }, 'afs.setPrice() is deprecated. Use act.setPrice() instead.'),
-  getPrice: util.deprecate(async (opts) => { return price.getPrice(opts) }, 'afs.getPrice() is deprecated. Use act.getPrice() instead.')
+  estimateSetPriceGasCost: util.deprecate(async (opts) => { return price.estimateSetPriceGasCost(opts) }, 'estimateSetPriceGasCost() is deprecated. Use estimateSetPriceGasCost() in ara-contracts instead.'),
+  removePriceTier: util.deprecate(async (opts) => { return price.removePriceTier(opts) }, 'removePriceTier() is deprecated. Use removePriceTier() in ara-contracts instead.'),
+  setPrice: util.deprecate(async (opts) => { return price.setPrice(opts) }, 'setPrice() is deprecated. Use setPrice() in ara-contracts instead.'),
+  getPrice: util.deprecate(async (opts) => { return price.getPrice(opts) }, 'getPrice() is deprecated. Use getPrice() in ara-contracts instead.')
 }
