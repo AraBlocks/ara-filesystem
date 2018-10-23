e1b02e2ce8747b231d9146e8970a4e3155363c6a
cckelly21 <cckelly21@gmail.com>
2018-10-23 12:09:09 -0400

    fix(): destroy estimate, minor bug fixes

--- destroy.js
+++ destroy.js
@@ -45,80 +45,88 @@ async function destroy(opts) {
   } else if ('string' !== typeof opts.did || !opts.did) {
     throw new TypeError('Expecting non-empty string.')
   } else if (opts.password && 'string' !== typeof opts.password) {
-    throw TypeError('Expecting non-empty password.')
+    throw new TypeError('Expecting non-empty password.')
+  } else if (opts.estimate && 'boolean' !== typeof opts.estimate) {
+    throw new TypeError('Expecting estimate to be a boolean.')
   }
 
+  const estimate = Boolean(opts.estimate)
   const { keyringOpts } = opts
   let { did } = opts
   did = normalize(did)
 
   let path
 
-  try {
-    // destroy AFS identity
-    path = createIdentityKeyPathFromPublicKey(did)
-    await pify(access)(path)
-    await pify(rimraf)(path)
-
-    // delete AFS on disk
-    path = createAFSKeyPath(did)
-    await pify(access)(path)
-    await pify(rimraf)(path)
-  } catch (err) {
-    throw err
-  }
-
-  const { store } = rc.afs.archive
-  path = resolvePath(store, basename(path))
-
-  // delete AFS toilet db file
-  try {
-    await pify(access)(path)
-    await pify(rimraf)(path)
-  } catch (err) {
-    debug('db file at %s does not exist', path)
-  }
-
-  const { password } = opts
-
-  if (password) {
+  if (!estimate) {
     try {
-      ({ did } = await validate({
-        did,
-        password,
-        label: 'destroy',
-        keyringOpts
-      }))
+      // destroy AFS identity
+      path = createIdentityKeyPathFromPublicKey(did)
+      await pify(access)(path)
+      await pify(rimraf)(path)
+
+      // delete AFS on disk
+      path = createAFSKeyPath(did)
+      await pify(access)(path)
+      await pify(rimraf)(path)
     } catch (err) {
       throw err
     }
 
-    if (!(await proxyExists(did))) {
-      debug('This content does not have a valid proxy contract.')
-      return
-    }
+    const { store } = rc.afs.archive
+    path = resolvePath(store, basename(path))
 
-    const afsDdo = await aid.resolve(did, keyringOpts)
-    let owner = getDocumentOwner(afsDdo, true)
-
-    owner = `${AID_PREFIX}${owner}`
-    const acct = await account.load({ did: owner, password })
-    const proxy = await getProxyAddress(did)
-
+    // delete AFS toilet db file
     try {
-      const transaction = await tx.create({
-        account: acct,
-        to: proxy,
-        data: {
-          abi,
-          name: 'unlist'
-        }
-      })
-      await tx.sendSignedTransaction(transaction)
+      await pify(access)(path)
+      await pify(rimraf)(path)
     } catch (err) {
-      throw new Error(err)
+      debug('db file at %s does not exist', path)
     }
   }
+
+  const { password } = opts
+
+  try {
+    ({ did } = await validate({
+      did,
+      password,
+      label: 'destroy',
+      keyringOpts
+    }))
+  } catch (err) {
+    throw err
+  }
+
+  if (!(await proxyExists(did))) {
+    throw new Error('Content does not have a valid proxy contract')
+  }
+
+  const afsDdo = await aid.resolve(did, keyringOpts)
+  let owner = getDocumentOwner(afsDdo, true)
+
+  owner = `${AID_PREFIX}${owner}`
+  const acct = await account.load({ did: owner, password })
+  const proxy = await getProxyAddress(did)
+
+  let transaction
+  try {
+    transaction = await tx.create({
+      account: acct,
+      to: proxy,
+      gasLimit: 1000000,
+      data: {
+        abi,
+        functionName: 'unlist'
+      }
+    })
+    if (estimate) {
+      return tx.estimateCost(transaction)
+    }
+  } catch (err) {
+    throw err
+  }
+
+  return tx.sendSignedTransaction(transaction)
 }
 
 module.exports = {
