2e0523734f5e019617ebc747507072bdef0819b1
cckelly21 <cckelly21@gmail.com>
2018-10-24 11:52:12 -0400

    refactor(): remove dedicated estimate functions

--- index.js
+++ index.js
@@ -4,30 +4,20 @@ const { destroy } = require('./destroy')
 const { create } = require('./create')
 const { remove } = require('./remove')
 const metadata = require('./metadata')
+const { commit } = require('./commit')
 const { add } = require('./add')
 
-const {
-  commit,
-  estimateCommitGasCost
-} = require('./commit')
-
-const {
-  setPrice,
-  getPrice,
-  estimateSetPriceGasCost
-} = require('./price')
+const { setPrice, getPrice } = require('./price')
 
 module.exports = {
-  create,
-  destroy,
-  add,
-  remove,
-  commit,
+  unarchive,
+  ownership,
   setPrice,
   getPrice,
-  estimateSetPriceGasCost,
-  estimateCommitGasCost,
-  unarchive,
   metadata,
-  ownership
+  destroy,
+  create,
+  remove,
+  commit,
+  add,
 }
